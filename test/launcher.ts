9cf901d61bc73c0c4a074f5408497f6d38d0aa5a
Carlos Martinez <carlos.crono@gmail.com>
2024-08-12 21:15:37 -0600

    Delete Item

--- test/launcher.ts
+++ test/launcher.ts
@@ -5,13 +5,13 @@ process.env.TABLE_NAME = "SpaceTable-0e0609817685";
 
 handler(
   {
-    httpMethod: "PUT",
+    httpMethod: "DELETE",
     queryStringParameters: {
       id: "5fc1b1d0-a954-4056-ae54-0f83356de137",
     },
-    body: JSON.stringify({
-      location: "San Salvador",
-    }),
+    // body: JSON.stringify({
+    //   location: "San Salvador",
+    // }),
   } as any,
   {} as any
 );
