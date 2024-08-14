435532a39069b8653363002a3d6df582d68d9054
Carlos Martinez <carlos.crono@gmail.com>
2024-08-14 05:57:52 -0600

    Generating tokens with AWS Amplify

---
+++ test/auth.test.ts
@@ -0,0 +1,9 @@
+import { AuthService } from "../src/services/auth/AuthService";
+
+async function testAuth() {
+  const service = new AuthService();
+  const loginResult = await service.login("carloscrono", "sdf45Aaf574fg$");
+  const idToken = await service.getIdToken();
+}
+
+testAuth();
