7355c2a7afa86baea3a29c0cfb605431f78f0655
Carlos Martinez <carlos.crono@gmail.com>
2024-08-18 10:11:06 -0600

    Using AWS insinde a React project with Amplify

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -10,10 +10,12 @@ const dataStack = new DataStack(app, "DataStack");
 const lambdaStack = new LambdaStack(app, "LambdaStack", {
   spacesTable: dataStack.spacesTable,
 });
-const authStack = new AuthStack(app, "AuthStack");
+const authStack = new AuthStack(app, "AuthStack", {
+  photosBucket: dataStack.photosBucket,
+});
 new ApiStack(app, "ApiStack", {
   spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
   userPool: authStack.userPool,
 });
 
-new UIDeploymentStack(app, 'UIDeploymentStack');
+new UIDeploymentStack(app, "UIDeploymentStack");
