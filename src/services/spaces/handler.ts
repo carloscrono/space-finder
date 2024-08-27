813cf20ca9b0ae4bc3bfafb6817d084d0ada5bcc
carloscrono <carlos.crono@gmail.com>
2024-08-27 13:54:22 -0600

    AWS X-RAY P2

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -11,8 +11,7 @@ import { updateSpace } from "./UpdateSpace";
 import { deleteSpace } from "./DeleteSpace";
 import { JsonError, MissingFieldError } from "../shared/Validator";
 import { addCorsHeader } from "../shared/Utils";
-import { captureAWSv3Client } from 'aws-xray-sdk-core'
-import { resolve } from "path";
+import { captureAWSv3Client, getSegment } from 'aws-xray-sdk-core'
 
 const ddbClient = captureAWSv3Client(new DynamoDBClient({}));
 
@@ -23,6 +22,14 @@ async function handler(
   context: Context
 ): Promise<APIGatewayProxyResult> {
   let response: APIGatewayProxyResult;
+
+  const subSeg = getSegment().addNewSubsegment('MyLongCall');
+  await new Promise(resolve => { setTimeout(resolve, 3000) });
+  subSeg.close();
+
+  const subSeg2 = getSegment().addNewSubsegment('MyLongCall');
+  await new Promise(resolve => { setTimeout(resolve, 500) });
+  subSeg2.close();
 
   try {
     switch (event.httpMethod) {
