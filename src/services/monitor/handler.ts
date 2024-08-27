fbe20e0e652283a75a7e91d190f9bdcdcbd9a60c
Carlos Martinez <carlos.crono@gmail.com>
2024-08-27 13:30:26 -0600

    SNS test

--- src/services/monitor/handler.ts
+++ src/services/monitor/handler.ts
@@ -1,16 +1,17 @@
 import { SNSEvent } from "aws-lambda";
 
-const webHookUrl = 'https://hooks.slack.com/services/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';
+const webHookUrl =
+  "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
 
 async function handler(event: SNSEvent, context) {
-    for (const record of event.Records) {
-        await fetch(webHookUrl, {
-            method: 'POST',
-            body: JSON.stringify({
-                "text": `Huston, we have a problem: ${record.Sns.Message}`
-            })
-        });
-    }
+  for (const record of event.Records) {
+    await fetch(webHookUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        text: `Huston, we have a problem: \`\`\`${record.Sns.Message}\`\`\``,
+      }),
+    });
+  }
 }
 
-export { handler }
+export { handler };
