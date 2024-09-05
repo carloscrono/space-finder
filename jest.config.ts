3eef7de112c6350f86b9e2833f1281c522088899
carloscrono <carlos.crono@gmail.com>
2024-09-05 17:23:09 -0600

    test spie

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,14 @@
-import { Config } from '@jest/types';
+import { Config } from '@jest/types'
 
-const baseTestDir = '<rootDir>/test/infra';
+const baseTestDir = '<rootDir>/test/services';
+
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: [
         `${baseTestDir}/**/*test.ts`
     ]
+
 }
 
 export default config;
