9c64d4d481c902bf18eba3dd98818f1fa3c7627b
Carlos Martinez <carlos.crono@gmail.com>
2024-09-06 13:59:09 -0600

    Fixing test case

--- test/services/handler.test.ts
+++ test/services/handler.test.ts
@@ -1,39 +1,44 @@
-import { handler } from "../../src/services/monitor/handler"
+import { handler } from "../../src/services/monitor/handler";
 
+describe("Monitor lambda tests", () => {
+  const fetchSpy = jest.spyOn(global, "fetch");
+  fetchSpy.mockImplementation(() => Promise.resolve({} as any));
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-describe('Monitor lambda tests', () => {
+  test("makes requests for records in SnsEvents", async () => {
+    await handler(
+      {
+        Records: [
+          {
+            Sns: {
+              Message: "Test message",
+            },
+          },
+        ],
+      } as any,
+      {}
+    );
 
-    const fetchSpy = jest.spyOn(global, 'fetch');
-    fetchSpy.mockImplementation(() => Promise.resolve({} as any));
-
-    afterEach(() => {
-        jest.clearAllMocks();
-    })
-
-    test('makes requests for records in SnsEvents', async () => {
-        await handler({
-            Records: [{
-                Sns: {
-                    Message: 'Test message'
-                }
-            }]
-        } as any, {});
-
-        expect(fetchSpy).toHaveBeenCalledTimes(1);
-        expect(fetchSpy).toHaveBeenCalledWith(expect.any(String), {
-            method: 'POST',
-            body: JSON.stringify({
-                "text": `Huston, we have a problem: Test message`
-            })
-        })
+    expect(fetchSpy).toHaveBeenCalledTimes(1);
+    expect(fetchSpy).toHaveBeenCalledWith(expect.any(String), {
+      method: "POST",
+      body: JSON.stringify({
+        text: "Huston, we have a problem: ```Test message```",
+      }),
     });
+  });
 
-    test('No sns records, no requests', async () => {
-        await handler({
-            Records: []
-        } as any, {});
+  test("No sns records, no requests", async () => {
+    await handler(
+      {
+        Records: [],
+      } as any,
+      {}
+    );
 
-        expect(fetchSpy).not.toHaveBeenCalled();
-    })
-})
+    expect(fetchSpy).not.toHaveBeenCalled();
+  });
+});
